decf5922bf7742a0e4834f5afb2829aaff19cfc4
horsegirl224 <jasmynr321@gmail.com>
2024-04-25 22:59:57 -0700

    Lab 5

---
+++ lab5/js/lab copy.js
@@ -0,0 +1,20 @@
+/** 
+* Author: Jasmyn Romo 
+* Created: April 25
+**/
+
+// Define Variables
+make = "Kia";
+model = "Soul";
+color = "Red";
+year = 2013;
+
+// calculate
+age = 2024 - year;
+
+// output
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age: " + age + " years<br>");
