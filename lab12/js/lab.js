a8becd6a0e43de96ddead038b9ef60fc01b72586
horsegirl224 <jasmynr321@gmail.com>
2024-05-28 18:28:10 -0700

    lab 12

---
+++ lab12/js/lab.js
@@ -0,0 +1,33 @@
+/*
+   lab.js - Using conditionals, I will create a Sorting Hat
+
+   Author: Jasmyn Romo
+   Date: 2023
+*/
+
+// Return Gryffindor, Ravenclaw, Slytherin, and Hufflepuff
+// depending on length mod 4
+function sortingHat(str) {
+  len = str.length;
+  mod = len % 4;
+  if (mod == 0) {
+    return "Gryffindor"
+  }
+  else if (mod == 1) {
+    return "Ravenclaw"
+  }
+  else if (mod == 2) {
+    return "Slytherin"
+  }
+  else if (mod == 3) {
+    return "Hufflepuff"
+  }
+}
+
+var myButton = document.getElementById("button");
+myButton.addEventListener("click", function() {
+    var name = document.getElementById("input").value;
+    var house = sortingHat(name);
+    newText = "<p>The Sorting Hat has sorted you into " + house + "</p>"
+    document.getElementById("output").innerHTML = newText;
+})
